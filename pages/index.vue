d70b7251b6d3087c46bdf851afac265931bc1e02
alex <alex>
2020-07-03 14:42:20 +0800

    初始化nuxtjs代码

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div class="row">这里是首页</div>
+    <div>{{ title }}</div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Logo from "~/components/Logo.vue";
+export default {
+  components: { Logo },
+  async asyncData(app) {
+    let res = await axios({
+      url:
+        "https://hq.sinajs.cn/rn=1593671040453&list=s_sh000001,s_sz399001,s_sh000300,s_sz399415,s_sz399006"
+    });
+    res.data = res.data
+      .replace(/(var hq_str_s_.+?=".+?,)|\n|\r|"/gm, "")
+      .replace(/(\W\w\s\S|;)$/gm, "")
+      .split(";");
+    console.log(res.data);
+    return {
+      title: res.data
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.container {
+  .row {
+    background: red;
+    width: 100px;
+    height: 100px;
+  }
+}
+</style>
